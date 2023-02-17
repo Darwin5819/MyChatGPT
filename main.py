f88aa9b2c66b711fa1180bc8a607d098513cd9bc
Darwin Maulik <darwinmaulik143@gmail.com>
2023-02-17 21:17:33 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import openai
+
+openai.api_key = "Your secret API key"
+
+while True:
+    model_engine = "text-davinci-003"
+    prompt = input("Enter new Prompt :")
+    if "exit" in prompt or "quit" in prompt:
+        break
+
+    completion = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5)
+
+    response = completion.choices[0].text
+
+    print(response)
